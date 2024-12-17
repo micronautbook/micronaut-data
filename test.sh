095399e2a0a3b820d5e337023ae5f9211c3b334f
Sergio del Amo <sergio.delamo@softamo.com>
2024-12-17 15:11:35 +0100

    multiple-database

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+EXIT_STATUS=0
+
+modules=("micronaut-data-jdbc-multiple-databases")
+
+for module in "${modules[@]}"; do
+  cd "$module" || exit 1
+  ./gradlew build || EXIT_STATUS=$?
+  if [ $EXIT_STATUS -ne 0 ]; then
+    exit $EXIT_STATUS
+  fi
+#  ./gradlew nativeTest || EXIT_STATUS=$?
+#  if [ $EXIT_STATUS -ne 0 ]; then
+#    exit $EXIT_STATUS
+#  fi
+  cd ..
+done
+
+exit $EXIT_STATUS
